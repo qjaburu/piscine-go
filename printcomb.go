77723135fa2f7a0fe92dd687170d42321f4fe7a3
quochieng <aiyanarich64@gmail.com>
2024-01-25 09:30:58 +0300

    combprint

--- printcomb.go
+++ printcomb.go
@@ -1,25 +1,24 @@
-package main 
+package piscine
 
 import "github.com/01-edu/z01"
- 
+
 func PrintComb() {
+	set := false
+	for x := '0'; x <= '9'; x++ {
+		for y := x + 1; y <= '9'; y++ {
+			for z := y + 1; z <= '9'; z++ {
 
-	for x := '0' ; x <= '7' ; x++ {
-
-		for y := x+1 ; y<= '8' ; y++ {
-
-			for z :=y+1 ; y<= '9' ; z++ {
-
-			z01.PrintComb(x)
-		   z01.PrintComb(y)
-		   z01.PrintComb(z)
-
-
-		   if x != '7' || y != '8' || z != '9'{
-			z01.PrintRune(',')
-			z01.PrintRune('')
+				if set {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
+				set = true
+				z01.PrintRune(x)
+				z01.PrintRune(y)
+				z01.PrintRune(z)
 			}
 		}
 	}
+
 	z01.PrintRune('\n')
 }
