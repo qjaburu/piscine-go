8fff3d5f15fadaa0de817419abc96e6e59285f94
quochieng <aiyanarich64@gmail.com>
2024-01-27 00:36:01 +0300

    Strevs

--- strrev.go
+++ strrev.go
@@ -1,11 +1,11 @@
 package piscine
 
 func StrRev(s string) string {
-	runes := []rune(s)
+	letters := []rune(s)
 
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	for i, j := 0, len(letters)-1; i < j; i, j = i+1, j-1 {
+		letters[i], letters[j] = letters[j], letters[i]
 	}
 
-	return string(runes)
+	return string(letters)
 }
