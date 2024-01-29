2b126193becea8aa75572367e5d75e9b0f2d3f29
quochieng <aiyanarich64@gmail.com>
2024-01-29 22:42:36 +0300

    updated recursiveFactorial

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,11 +1,10 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 { // this test if this is negative number
+	if nb < 0 || nb > 20 {
 		return 0
-	} else if nb == 0 || nb == 1 {
+	} else if nb == 0 {
 		return 1
-	} else {
-		return nb * RecursiveFactorial(nb-1)
 	}
+	return nb * RecursiveFactorial(nb-1)
 }
