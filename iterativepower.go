c30fd40c0d09d544f83c9ab4841880d93fbd1aab
quochieng <aiyanarich64@gmail.com>
2024-01-29 22:56:01 +0300

    iteractivepower

---
+++ iterativepower.go
@@ -0,0 +1,16 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	result := 1
+	if power < 0 {
+		return 0
+	}
+	if power == 0 {
+		return 1
+	}
+	for power > 1 {
+		result *= nb
+		power--
+	}
+	return result
+}
