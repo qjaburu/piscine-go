0a954585ace332e80fb9a81e4b52ebd4eac26589
quochieng <aiyanarich64@gmail.com>
2024-01-29 16:33:24 +0300

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,17 +1,19 @@
-package piscine
+package main
 
 func IterativeFactorial(nb int) int {
 	if nb < 0 {
 		return 0 // returns when n is negative
-	} else {
-		result := 1
-		for i := 1; i <= nb; i++ {
-			if result*i < result {
-				return 0
-			}
-			result *= i
-		}
-		return result
-
 	}
+	result := 1
+	for i := 1; i <= nb; i++ {
+		if result*i < result {
+			return 0
+		}
+		result *= i
+	}
+	return result
+}
+
+func main() {
+	IterativeFactorial(4)
 }
