5883329edfc4c0c429d4df5d125bcf5ffa62fc05
quochieng <aiyanarich64@gmail.com>
2024-01-30 21:56:48 +0300

    recursivepower

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -1,10 +1,10 @@
 package piscine
 
 func RecursiveFactorial(nb int) int {
-	if nb < 0 || power == 0 {
+	if nb < 0 || nb < 20 {
 		return 0
 	} else if nb == 0 {
 		return 1
 	}
-	return nb * iterativePower (nb-1 ) )
+	return nb * RecursiveFactorial(nb-1)
 }
---
+++ recursivepower.go
@@ -0,0 +1,9 @@
+package piscine
+
+func RecursivePower(nb int, power int) int {
+	if power == 0 {
+		return 1
+	} else {
+		return nb * RecursivePower(nb, power-1)
+	}
+}
