a0aedffb13acf8f9d9bc8e64c6b382c3dabfec1b
quochieng <aiyanarich64@gmail.com>
2024-01-31 01:28:41 +0300

    sqrt.go

---
+++ sqrt.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func Sqrt(nb int) int {
+	if nb <= 0 {
+		return 0
+	}
+	for i := 0; i*i <= nb; i++ {
+		if i*i == nb {
+			return i
+		}
+	}
+	return 0
+}
+
+func main() {
+	fmt.Println(Sqrt(4))
+	fmt.Println(Sqrt(3))
+}
