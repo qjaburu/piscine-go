8fd634a455d54b0614097a4668f1ea3e73e31134
quochieng <aiyanarich64@gmail.com>
2024-02-01 01:58:51 +0300

    alphacount

---
+++ alphacount.go
@@ -0,0 +1,12 @@
+package main
+
+func AlphaCount(s string) int {
+	count := 0
+	for _, char := range s {
+		if char >= 'A' && char <= 'Z' || char >= 'a' && char <= 'z' {
+			count++
+		}
+		return count
+	}
+	return 0
+}
