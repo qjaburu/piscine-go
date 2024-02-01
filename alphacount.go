950f1ed26f185c4715f937c68cfb5b4aa02eee2d
quochieng <aiyanarich64@gmail.com>
2024-02-01 09:52:24 +0300

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -1,12 +1,11 @@
 package piscine
 
 func AlphaCount(s string) int {
-	var count int
+	count := 0
 	for _, char := range s {
 		if (char >= 'A' && char <= 'Z') || (char >= 'a' && char <= 'z') {
 			count++
 		}
-		return count
 	}
-	return 0
+	return count
 }
