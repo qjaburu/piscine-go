845a39ac5e020332bc056be8c11ec05c8707d96c
quochieng <aiyanarich64@gmail.com>
2024-02-02 23:05:13 +0300

    isalpha

---
+++ isalpha.go
@@ -0,0 +1,15 @@
+package main
+
+func IsAlpha(s string) bool {
+	if len(s) == 0 {
+		return true
+	}
+	for _, r := range s {
+		if (r >= 'a' && r <= 'z') || (r >= 'A' && r <= 'Z') || (r >= '0' && r <= '9') {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
