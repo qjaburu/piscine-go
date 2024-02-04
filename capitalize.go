356f78906c00063d6fa4bc6231e10370b2890a76
quochieng <aiyanarich64@gmail.com>
2024-02-04 04:41:07 +0300

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -4,7 +4,7 @@ func Capitalize(s string) string {
 	var result string
 	var next bool
 	for _, n := range s {
-		if (n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'z') || (n >= '0' && n <= '9') {
+		if (n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'Z') || (n >= '0' && n <= '9') {
 			if next {
 				if n >= 'a' && n <= 'z' {
 					result += string(n - ('a' - 'A'))
@@ -12,12 +12,11 @@ func Capitalize(s string) string {
 					result += string(n)
 				}
 				next = false
-
 			} else {
 				if n >= 'A' && n <= 'Z' {
-					result += string(n + ('a' - 'A'))
-				} else {
 					result += string(n)
+				} else {
+					result += string(n + ('a' - 'A'))
 				}
 			}
 		} else {
