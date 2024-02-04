36c457ae0e589f800df8ec090a712ca330a8d8b5
quochieng <aiyanarich64@gmail.com>
2024-02-04 03:16:37 +0300

    capitalize

--- capitalize.go
+++ capitalize.go
@@ -1,31 +1,29 @@
 package piscine
 
-
 func Capitalize(s string) string {
 	var result string
 	var next bool
 	for _, n := range s {
-		if (n >= 'a' &&n <= 'z') || (n >='A' && n <= 'z')  || (n >= '0' && n <= '9'){
+		if (n >= 'a' && n <= 'z') || (n >= 'A' && n <= 'z') || (n >= '0' && n <= '9') {
 			if next {
-				if n >= 'a' && n <= 'z'{
-					result += string(n-32)
+				if n >= 'a' && n <= 'z' {
+					result += string(n - 32)
 				} else {
 					result += string(n)
 				}
-			}if next {
-				if n >= 'A' && n<= 'Z'{
+				next = false
+
+			} else {
+				if n >= 'A' && n <= 'Z' {
 					result += string(n + 32)
-				} else{
+				} else {
 					result += string(n)
 				}
-
 			}
-		}else {
+		} else {
 			result += string(n)
-			new = true
+			next = true
 		}
 	}
 	return result
-}   
-
-
+}
