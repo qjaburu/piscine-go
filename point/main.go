ab3c9a927446ee1c752a80b109387baf191234de
quochieng <aiyanarich64@gmail.com>
2024-02-05 20:24:50 +0300

    point

---
+++ point/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "github.com/01-edu/z01"
+
+type point struct {
+	x, y int
+}
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+
+	z01.PrintRune('x')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.Printf("%d", points.x)
+	z01.PrintRune(',')
+	z01.PrintRune(' ')
+	z01.PrintRune('y')
+	z01.PrintRune(' ')
+	z01.PrintRune('=')
+	z01.PrintRune(' ')
+	z01.Printf("%d", points.y)
+	z01.PrintRune('\n')
+}
