e40694803ba8c24d4f758f78782dfc118c6de874
quochieng <aiyanarich64@gmail.com>
2024-02-07 11:56:02 +0300

    descentcomb

--- descendcomb.go
+++ descendcomb.go
@@ -1,24 +1,30 @@
-package piscine
+package main
 
 import "github.com/01-edu/z01"
 
 func DescendComb() {
 	for a := '9'; a >= '0'; a-- {
 		for b := '9'; b >= '0'; b-- {
+			d := b - 1
 			for c := '9'; c >= '0'; c-- {
-				for d := '9'; d >= '0'; d-- {
+				for ; d >= '0'; d-- {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
 					z01.PrintRune(' ')
 					z01.PrintRune(c)
 					z01.PrintRune(d)
 
-					if a > '0' || b > '0' || c > '0' || d > '0' {
+					if a > '0' || b > '1' || c > '0' || d > '0' {
 						z01.PrintRune(',')
 						z01.PrintRune(' ')
 					}
 				}
+				d = '9'
 			}
 		}
 	}
+}
+
+func main() {
+	DescendComb()
 }
