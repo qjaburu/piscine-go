959041bd139f8cf35e1b6fef921c85d52a5edfff
quochieng <aiyanarich64@gmail.com>
2024-02-07 15:26:06 +0300

    abort

--- abort.go
+++ abort.go
@@ -3,7 +3,7 @@ package piscine
 func Abort(a, b, c, d, e int) int {
 	store := []int{a, b, c, d, e}
 	for i := 0; i < len(store); i++ {
-		for j := i + 1; j < len(store); i++ {
+		for j := i + 1; j < len(store); j++ {
 			if store[j] < store[i] {
 				store[i], store[j] = store[j], store[i]
 			}
