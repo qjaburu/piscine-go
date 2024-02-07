56de9f96e850a09ec5bf4470b5734198c91fde9e
quochieng <aiyanarich64@gmail.com>
2024-02-07 23:28:43 +0300

    descendappend

---
+++ descendappendrange.go
@@ -0,0 +1,13 @@
+package piscine
+
+func DescendAppendRange(max, min int) []int {
+	if max < min {
+		return []int{}
+	}
+	var result []int
+	for i := max; i > min+1; i-- {
+		result = append(result, i)
+	}
+
+	return result
+}
