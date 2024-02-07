e4605b692d221db9661fc659761e2629b613d19e
quochieng <aiyanarich64@gmail.com>
2024-02-07 21:59:07 +0300

    dealpack

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -7,7 +7,7 @@ func DealAPackOfCards(deck []int) {
 	Player := 1
 	for i := 0; i < 12; i += 3 {
 
-		fmt.Printf("Player %v: %v,%v, %v\n", Player, rangDeck[i], rangDeck[i+1], rangDeck[i+2])
+		fmt.Printf("Player %v: %v, %v, %v\n", Player, rangDeck[i], rangDeck[i+1], rangDeck[i+2])
 		Player++
 	}
 }
