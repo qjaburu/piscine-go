1a0b65702d1d818f6b77b0bf70510388f62db732
quochieng <aiyanarich64@gmail.com>
2024-02-08 01:20:15 +0300

    comheckmain

---
+++ comcheck/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args[1:]
+	for _, a := range args {
+		if a == "01" || a == "galaxy" || a == "galaxy 01" {
+			fmt.Println("Alert!!!")
+			return
+		}
+	}
+}
