42a9c7bd4d4e44df939d426e2123dd136a4dcfe7
quochieng <aiyanarich64@gmail.com>
2024-02-07 14:24:13 +0300

    collazcount

---
+++ collatzcountdown.go
@@ -0,0 +1,18 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	if start <= 0 {
+		return -1
+	}
+	nsteps := 0
+	if start != 1 {
+		if start%2 == 0 {
+			start = start / 2
+		} else {
+			start = 3*start + 1
+		}
+		nsteps++
+
+	}
+	return start
+}
