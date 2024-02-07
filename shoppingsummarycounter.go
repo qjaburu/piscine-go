fec2be7c33cc630ad3fe305ebd0930985fcea6f2
quochieng <aiyanarich64@gmail.com>
2024-02-07 16:34:02 +0300

    shopping  summary

--- shoppingsummarycounter.go
+++ shoppingsummarycounter.go
@@ -4,10 +4,11 @@ func ShoppingSummaryCounter(str string) map[string]int {
 	list := make(map[string]int)
 
 	var items string
+
 	for _, el := range str { // el is shorr form of element
 		if el == 32 {
 			list[items] += 1
-			items = " "
+			items = ""
 
 		} else if el != 32 {
 			items += string(byte(el))
