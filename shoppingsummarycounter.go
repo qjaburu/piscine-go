0b4854ec17ca911115a8c13b74cfecc336007a72
quochieng <aiyanarich64@gmail.com>
2024-02-07 16:32:10 +0300

    shopping  summary

---
+++ shoppingsummarycounter.go
@@ -0,0 +1,18 @@
+package piscine
+
+func ShoppingSummaryCounter(str string) map[string]int {
+	list := make(map[string]int)
+
+	var items string
+	for _, el := range str { // el is shorr form of element
+		if el == 32 {
+			list[items] += 1
+			items = " "
+
+		} else if el != 32 {
+			items += string(byte(el))
+		}
+		list[items] += 1
+	}
+	return list
+}
