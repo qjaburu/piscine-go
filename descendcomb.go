c268ab28d44204d65887feaf396c21e4e64d931e
quochieng <aiyanarich64@gmail.com>
2024-02-07 13:58:23 +0300

    descentcomb

--- descendcomb.go
+++ descendcomb.go
@@ -6,7 +6,7 @@ func DescendComb() {
 	for a := '9'; a >= '0'; a-- {
 		for b := '9'; b >= '0'; b-- {
 			d := b - 1
-			for c := 'a'; c >= '0'; c-- {
+			for c := a; c >= '0'; c-- {
 				for ; d >= '0'; d-- {
 					z01.PrintRune(a)
 					z01.PrintRune(b)
