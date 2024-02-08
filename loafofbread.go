a4a4c70f1a9c3d93b9e509669da24985ee39dcb8
quochieng <aiyanarich64@gmail.com>
2024-02-08 09:15:12 +0300

    loafofbread

---
+++ loafofbread.go
@@ -0,0 +1,21 @@
+package main
+
+func LoafOfBread(s string) string {
+	if len(s) < 5 {
+		return "Invalid Output\n"
+	}
+	var res string
+	for i := 0; i < len(s); i += 6 {
+		if i+5 >= len(s) {
+			res += s[i:]
+			break
+		}
+		if s[i+5] == ' ' {
+			res += s[i:i+5] + " "
+			i--
+		} else {
+			res += s[i : i+5]
+		}
+	}
+	return res
+}
