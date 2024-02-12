6ecdfdf233039119da2417cd4db5007ee28604d2
quochieng <aiyanarich64@gmail.com>
2024-02-12 22:58:46 +0300

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,22 +3,18 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	set := false
 	for x := '0'; x <= '9'; x++ {
 		for y := x + 1; y <= '9'; y++ {
 			for z := y + 1; z <= '9'; z++ {
-
-				if set {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				}
-				set = true
 				z01.PrintRune(x)
 				z01.PrintRune(y)
 				z01.PrintRune(z)
+				if x != '7' && y != 8 && z != 9 {
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				}
 			}
 		}
 	}
-
 	z01.PrintRune('\n')
 }
