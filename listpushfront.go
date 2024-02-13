da93700e8cfa20d91960e2190569f455be230ae9
quochieng <aiyanarich64@gmail.com>
2024-02-13 11:35:34 +0300

    listpushfront

---
+++ listpushfront.go
@@ -0,0 +1,23 @@
+package piscine 
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListPushFront(l *List, data interface{}) {
+	node := &NodeL{Data: data}
+	if l.Head= nil{
+		l.Head = node
+		return
+	}
+	l.Tail.Next=l.Head
+	l.Head = node
+
+}
+
