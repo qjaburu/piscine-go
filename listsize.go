8a14c9108db3fe1329bf9cd43907e5e686b6b092
quochieng <aiyanarich64@gmail.com>
2024-02-13 20:40:00 +0300

    size

--- listsize.go
+++ listsize.go
@@ -10,16 +10,6 @@ type List struct {
 	Tail *NodeL
 }
 
-func ListPushFront(l *List, data interface{}) {
-	node := &NodeL{Data: data}
-	if l.Head == nil {
-		l.Head = node
-		return
-	}
-	node.Next = l.Head
-	l.Head = node
-}
-
 func ListSize(l *List) int {
 	size := 0
 	current := l.Head
