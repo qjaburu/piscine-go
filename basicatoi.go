1c956108307b87ff7bba542fcc896c1977629c2f
quochieng <aiyanarich64@gmail.com>
2024-02-13 22:40:28 +0300

    basicatoi

--- basicatoi.go
+++ basicatoi.go
@@ -3,7 +3,7 @@ package piscine
 func BasicAtoi(s string) int {
 	result := 0
 	for _, char := range s {
-		result = result*10 - int(char-'0')
+		result = result*10 + int(char-'0')
 	}
 	return result
 }
