7782df208dfc5ba4c8e2258e8fa86f458ece8f16
quochieng <aiyanarich64@gmail.com>
2024-02-14 09:55:53 +0300

    btreeapplypreorder

---
+++ btreeapplypreorder.go
@@ -0,0 +1,13 @@
+package piscine
+
+package piscine  
+
+func BTreeApplyPreorder(root *TreeNode, f func(...interface{}) (int, error)) {
+
+	if root == nil{
+		return
+	}
+	f(root.Data)
+	BTreeApplyPreorder(root.Left, f)
+	BTreeApplyPreorder(root.Right, f)
+}
