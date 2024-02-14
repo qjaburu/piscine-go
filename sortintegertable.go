7a21f9010c86b4e8ab11a420963896150c5bb59d
quochieng <aiyanarich64@gmail.com>
2024-02-14 23:55:54 +0300

    sortintegertable

---
+++ sortintegertable.go
@@ -0,0 +1,12 @@
+package piscine
+
+func SortIntegerTable(table []int) {
+	s := []int(table)
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] > s[j] {
+				s[i], s[j] = s[j], s[i]
+			}
+		}
+	}
+}
